4a68645771fe8c30fc84b781138d047dcef2d7c3
Eleaus-Hossain-Evan <eleaus16-364@diu.edu.bdm>
2023-06-14 02:35:50 +0600

    change on map widget

--- lib/presentation/order/widgets/google_map_widget.dart
+++ lib/presentation/order/widgets/google_map_widget.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:be_gelled/utils/assets/resources.dart';
 
